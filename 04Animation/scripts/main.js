7b7cac4fcfff8af89661566ab33961512d034edb
malouhermans <61313164+malouhermans@users.noreply.github.com>
2020-05-19 15:09:39 +0200

    Add files via upload

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,12 +1,15 @@
+// changing individual properties with code and using setInterval
+var rotationSpeed = 0.05;
+var myOtherBox = document.getElementById('myOtherBox');
 
+function spin(){
+	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.y);
+	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.z);
+}
 
-// // changing individual properties with code and using setInterval
-// var rotationSpeed = 0.01;
-// var myOtherBox = document.getElementById('myOtherBox');
+setInterval(spin, 16); //equivalent to 60 fps
 
-// function spin(){
-// 	myOtherBox.object3D.rotation.x += rotationSpeed;
-// 	console.log(myOtherBox.object3D.rotation.x);
-// }
-
-// setInterval(spin, 16); //equivalent to 60 fps
