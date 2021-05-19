b2572b5c38a0bf8fc63711a537687970e11029fc
malouhermans <maloutje2@gmail.com>
2021-05-19 14:06:21 +0200

    Opdrachten 4.1 tot en met 4.6
    Binnen deze update worden alle opdrachten binnen AFrame 04 animation tot 4.6 geupdate.

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -1,17 +1,33 @@
 // changing individual properties with code and using setInterval
+
+
 var rotationSpeed = 0.05;
-// De balk sneller laten draaien
+// 4.6 De balk sneller laten draaien, dit draaide echter te snel, dus ik heb besloten weer met de oude 
+// waarde door te gaan:
 // var rotationSpeed = 0.50;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-	myOtherBox.object3D.rotation.x -= rotationSpeed;
+	// 4.4 Clockwise:
+	// myOtherBox.object3D.rotation.x -= rotationSpeed;
+	// console.log(myOtherBox.object3D.rotation.x);
+	// myOtherBox.object3D.rotation.y -= rotationSpeed;
+	// console.log(myOtherBox.object3D.rotation.y);
+	// myOtherBox.object3D.rotation.z -= rotationSpeed;
+	// console.log(myOtherBox.object3D.rotation.z);
+
+	// 4.4 Counter clockwise:
+	myOtherBox.object3D.rotation.x += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
-	myOtherBox.object3D.rotation.y -= rotationSpeed;
+	myOtherBox.object3D.rotation.y += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.y);
-	myOtherBox.object3D.rotation.z -= rotationSpeed;
+	myOtherBox.object3D.rotation.z += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.z);
+
+	// 4.5 Er zijn drie regels code geschreven, dit is om de verschillende assen te laten draaien
 }
 
 setInterval(spin, 16); //equivalent to 60 fps
+
+// 
 
